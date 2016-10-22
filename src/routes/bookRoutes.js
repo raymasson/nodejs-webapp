491c348b9d09d8fc004ac63b48995b67258013c6
Raymond MASSON <Raymond MASSON>
2016-10-22 21:39:48 +0200

    mongodb usage

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -1,46 +1,75 @@
 var express = require('express');
 
-var sql = require('mssql');
+//var sql = require('mssql');
+
+var mongodb = require('mongodb').MongoClient;
 
 var bookRouter = express.Router();
 
-var router = function(nav) {
-    var books = [{
-        title: 'War and Peace',
-        genre: 'Historical Fiction',
-        author: 'Lev NokiLayevich Tolstoy',
-        read: false
-    }, {
-        title: 'Les misérables',
-        genre: 'Historical',
-        author: 'Victor Hugo',
-        read: true
-    }];
+var ObjectId = require('mongodb').ObjectID;
 
+var router = function(nav) {
     bookRouter.route('/').get(function(req, res) {
         //res.send('Hello Books');
-        var request = new sql.Request();
-        request.query('select * from Book', function(err, recordset) {
-            console.log(recordset);
-        });
-
-        res.render('books', {
-            title: 'Books',
-            nav: nav,
-            books: books
-        });
-    });
-
-    bookRouter.route('/:id').get(function(req, res) {
-        //res.send('Hello single Book');
-        var id = req.params.id;
-
-        res.render('book', {
-            title: 'Book',
-            nav: nav,
-            book: books[id]
+        //var request = new sql.Request();
+        //request.query('select * from Book', function(err, recordset) {
+        //console.log(recordset);
+        //});
+        var url = 'mongodb://localhost:27017/NodeJsWebApp';
+        mongodb.connect(url, function(err, db) {
+            var collection = db.collection('books');
+            collection.find({}).toArray(function(err, results) {
+                res.render('books', {
+                    title: 'Books',
+                    nav: nav,
+                    books: results
+                });
+            });
         });
     });
+
+    bookRouter.route('/:id')
+        /*.all(function(req, res, next) {
+            var id = req.params.id;
+            var ps = new sql.PreparedStatement();
+            ps.input('BookId', sql.Int);
+            ps.prepare('select * from Book where BookId = @BookId', function(err) {
+                ps.execute({
+                    BookId: id
+                }, function(err, recordset) {
+                    if (recordset.length === 0) {
+                        res.status(404).send('Not Found');
+                    } else {
+                        req.book = recordset[0];
+                        next();
+                    }
+
+                    //console.log(recordset);
+                });
+            });
+        })*/
+        .get(function(req, res) {
+            //res.send('Hello single Book');
+            /*res.render('book', {
+                title: 'Book',
+                nav: nav,
+                book: req.book
+            });*/
+            var id = new ObjectId(req.params.id);
+            var url = 'mongodb://localhost:27017/NodeJsWebApp';
+            mongodb.connect(url, function(err, db) {
+                var collection = db.collection('books');
+                collection.findOne({
+                    _id: id
+                }, function(err, results) {
+                    res.render('book', {
+                        title: 'Book',
+                        nav: nav,
+                        book: results
+                    });
+                });
+            });
+        });
 
     return bookRouter;
 };
